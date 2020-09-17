a101525dec1b0495ce63cb3835e079133eafcf35
miguelgonch <miguelgon_8@hotmail.com>
2020-09-17 11:48:41 -0600

    limpieza de migrate contract

--- contracts/RequestProduct.sol
+++ contracts/RequestProduct.sol
@@ -4,18 +4,27 @@ contract RequestProduct {
     mapping(address=>int) _productRequests;
     int _productPrice;
     int _productId;
+    address _buyer = msg.sender;
     
+    //event Pago(address buyer, address seller, int productId, int quantity);
 
-    constructor(address buyer, int productId, int expiration) public {
+    constructor(address seller, int productId, int expiration,int quantity) public {
         // Realizar la consulta para verificar la disponiblidad del producto (Para mientras es un > 0, luego hay que hacer consulta a la db y ver si existe el producto)
+        int maxItems = 5;
         require(productId > 0,"Producto no es valido");
+        require(0 < quantity && quantity < maxItems,"No es una cantidad valida, revisar unidades disponibles y pedido minimo");
 
         _productId = productId;
     }
 
-    function purchase(int quantity) public {
+    // funcion de aceptar request por parte del seller
+    function sellerVote() public {
+        
+    }
+
+    // Ok del buyer
+    function buyerVote() public {
         // maxItems = consultar en db maximo dispobible
-        int maxItems = 5;
-        require(0 < quantity && quantity < maxItems,"No es una cantidad valida, revisar unidades disponibles y pedido minimo");
+
     }
 }
--- migrations/2_RequestProduct.js
+++ migrations/2_RequestProduct.js
@@ -1,5 +1,5 @@
 const RequestProduct = artifacts.require("RequestProduct");
 
 module.exports = function (deployer) {
-  deployer.deploy(RequestProduct());
+  deployer.deploy(RequestProduct,"0x3e0a39c5e8b68afb43d14b7a43a9e82ae37cb54f",2,5,1);
 };

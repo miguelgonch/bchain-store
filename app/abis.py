732aef3bfba5231533b72de0a0505c7dbe13bd64
mint <marcosmateom@gmail.com>
2020-10-15 22:41:09 -0600

    abis y funcionalidad de 3era entrega completa

--- app/abis.py
+++ app/abis.py
@@ -1,15 +1,4 @@
-import json
-import hashlib
-import pymongo
-import abis
-from web3 import Web3
-
-w3 = Web3(Web3.WebsocketProvider("ws://127.0.0.1:9000"))
-w3.eth.defaultAccount = w3.eth.accounts[0]
 false=False
-
-Store_address = "0xb634E8B98A7fc3022da023d37f0d778E28DF42c6"
-
 abi_product=[
 	{
 		"inputs": [
@@ -272,61 +261,3 @@ abi_request= [
 		"type": "receive"
 	}
 ]
-store_contract = w3.eth.contract(address=Store_address, abi=abi_store)
-
-#Coneccion con la db
-client = pymongo.MongoClient("mongodb+srv://dbUser:dbUser@cluster0.64zqb.mongodb.net/<dbname>?retryWrites=true&w=majority")
-db = client.productos
-
-#crear json de la descripcion del producto
-def createDescription(_nombre, _precio, _descripcion):
-    descripcion = {
-    "nombre": _nombre,
-    "precio": _precio,
-    "descripcion": _descripcion,
-    }
-    return descripcion
-
-def createDescriptionHash(_descripcion):
-    bDescripcion = json.dumps(_descripcion, sort_keys = True).encode("utf-8")
-    hashh = hashlib.md5(bDescripcion).hexdigest()
-    return hashh
-
-def createProduct(_nombre, _precio, _descripcion, _cantidad, db):
-    global store_contract
-    descripcion = createDescription(_nombre, _precio, _descripcion)
-    hashh = createDescriptionHash(descripcion)
-
-    producto = {
-    "hash": hashh,
-    "cantidad": _cantidad,
-    "descripcion": descripcion,
-    }
-    result=db.products.insert_one(producto)
-    print(result)
-    newProductFunction = store_contract.functions.newProduct(hashh,_cantidad,_precio).transact()
-    return hashh
-
-def checkHash(_hash):
-    res = db.products.find_one({ "descripcion.nombre": "OracleDB" })
-    print(res['hash'])
-    if res['hash']==_hash:
-        print('yay')    
-
-def deleteProduct(_hash, db):
-    result = db.products.delete_one({'hash': _hash})
-    if result:
-        print('Entrada Eliminada')
-
-def hi():
-    print('Hello!!')
-print('estas adentro!!!!')
-
-hashh = createProduct('Oracle', 10, 'Licencia pirateada de OracleDB', 2, db)
-
-#res = db.products.find_one({ "descripcion.nombre": "OracleDB" })
-#print(res['hash'])
-#if res['hash']==hashh:
-#    print('yay')
-
-#SendJSON

e02b5ed3694589777399a672ea6d53215cf3ae88
mint <marcosmateom@gmail.com>
2020-10-15 22:45:45 -0600

    abis en otro .py

--- app/Productos.py
+++ app/Productos.py
@@ -10,269 +10,270 @@ false=False
 
 Store_address = "0xb634E8B98A7fc3022da023d37f0d778E28DF42c6"
 
-abi_product=[
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "productHash",
-				"type": "string"
-			},
-			{
-				"internalType": "address",
-				"name": "owner",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "stockQuantity",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "price",
-				"type": "uint256"
-			},
-			{
-				"internalType": "contract Store",
-				"name": "storeContract",
-				"type": "address"
-			}
-		],
-		"stateMutability": "payable",
-		"type": "constructor"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": false,
-				"internalType": "address",
-				"name": "requestAddress",
-				"type": "address"
-			}
-		],
-		"name": "NewRequest",
-		"type": "event"
-	},
-	{
-		"inputs": [],
-		"name": "_owner",
-		"outputs": [
-			{
-				"internalType": "address payable",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "_price",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "_storeAddress",
-		"outputs": [
-			{
-				"internalType": "address payable",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "uint256",
-				"name": "quantity",
-				"type": "uint256"
-			}
-		],
-		"name": "requestProduct",
-		"outputs": [],
-		"stateMutability": "payable",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "address",
-				"name": "buyer",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "quantity",
-				"type": "uint256"
-			}
-		],
-		"name": "updateProductRequest",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	}
-]
+# abi_product=[
+# 	{
+# 		"inputs": [
+# 			{
+# 				"internalType": "string",
+# 				"name": "productHash",
+# 				"type": "string"
+# 			},
+# 			{
+# 				"internalType": "address",
+# 				"name": "owner",
+# 				"type": "address"
+# 			},
+# 			{
+# 				"internalType": "uint256",
+# 				"name": "stockQuantity",
+# 				"type": "uint256"
+# 			},
+# 			{
+# 				"internalType": "uint256",
+# 				"name": "price",
+# 				"type": "uint256"
+# 			},
+# 			{
+# 				"internalType": "contract Store",
+# 				"name": "storeContract",
+# 				"type": "address"
+# 			}
+# 		],
+# 		"stateMutability": "payable",
+# 		"type": "constructor"
+# 	},
+# 	{
+# 		"anonymous": false,
+# 		"inputs": [
+# 			{
+# 				"indexed": false,
+# 				"internalType": "address",
+# 				"name": "requestAddress",
+# 				"type": "address"
+# 			}
+# 		],
+# 		"name": "NewRequest",
+# 		"type": "event"
+# 	},
+# 	{
+# 		"inputs": [],
+# 		"name": "_owner",
+# 		"outputs": [
+# 			{
+# 				"internalType": "address payable",
+# 				"name": "",
+# 				"type": "address"
+# 			}
+# 		],
+# 		"stateMutability": "view",
+# 		"type": "function"
+# 	},
+# 	{
+# 		"inputs": [],
+# 		"name": "_price",
+# 		"outputs": [
+# 			{
+# 				"internalType": "uint256",
+# 				"name": "",
+# 				"type": "uint256"
+# 			}
+# 		],
+# 		"stateMutability": "view",
+# 		"type": "function"
+# 	},
+# 	{
+# 		"inputs": [],
+# 		"name": "_storeAddress",
+# 		"outputs": [
+# 			{
+# 				"internalType": "address payable",
+# 				"name": "",
+# 				"type": "address"
+# 			}
+# 		],
+# 		"stateMutability": "view",
+# 		"type": "function"
+# 	},
+# 	{
+# 		"inputs": [
+# 			{
+# 				"internalType": "uint256",
+# 				"name": "quantity",
+# 				"type": "uint256"
+# 			}
+# 		],
+# 		"name": "requestProduct",
+# 		"outputs": [],
+# 		"stateMutability": "payable",
+# 		"type": "function"
+# 	},
+# 	{
+# 		"inputs": [
+# 			{
+# 				"internalType": "address",
+# 				"name": "buyer",
+# 				"type": "address"
+# 			},
+# 			{
+# 				"internalType": "uint256",
+# 				"name": "quantity",
+# 				"type": "uint256"
+# 			}
+# 		],
+# 		"name": "updateProductRequest",
+# 		"outputs": [],
+# 		"stateMutability": "nonpayable",
+# 		"type": "function"
+# 	}
+# ]
 
-abi_store = [
-	{
-		"inputs": [
-			{
-				"internalType": "address payable",
-				"name": "admin",
-				"type": "address"
-			}
-		],
-		"stateMutability": "payable",
-		"type": "constructor"
-	},
-	{
-		"anonymous": false,
-		"inputs": [
-			{
-				"indexed": false,
-				"internalType": "string",
-				"name": "productHash",
-				"type": "string"
-			},
-			{
-				"indexed": false,
-				"internalType": "address",
-				"name": "productAddress",
-				"type": "address"
-			}
-		],
-		"name": "NewProduct",
-		"type": "event"
-	},
-	{
-		"inputs": [],
-		"name": "_admin",
-		"outputs": [
-			{
-				"internalType": "address payable",
-				"name": "",
-				"type": "address"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "productHash",
-				"type": "string"
-			},
-			{
-				"internalType": "uint256",
-				"name": "stockQuantity",
-				"type": "uint256"
-			},
-			{
-				"internalType": "uint256",
-				"name": "price",
-				"type": "uint256"
-			}
-		],
-		"name": "newProduct",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	}
-]
+# abi_store = [
+# 	{
+# 		"inputs": [
+# 			{
+# 				"internalType": "address payable",
+# 				"name": "admin",
+# 				"type": "address"
+# 			}
+# 		],
+# 		"stateMutability": "payable",
+# 		"type": "constructor"
+# 	},
+# 	{
+# 		"anonymous": false,
+# 		"inputs": [
+# 			{
+# 				"indexed": false,
+# 				"internalType": "string",
+# 				"name": "productHash",
+# 				"type": "string"
+# 			},
+# 			{
+# 				"indexed": false,
+# 				"internalType": "address",
+# 				"name": "productAddress",
+# 				"type": "address"
+# 			}
+# 		],
+# 		"name": "NewProduct",
+# 		"type": "event"
+# 	},
+# 	{
+# 		"inputs": [],
+# 		"name": "_admin",
+# 		"outputs": [
+# 			{
+# 				"internalType": "address payable",
+# 				"name": "",
+# 				"type": "address"
+# 			}
+# 		],
+# 		"stateMutability": "view",
+# 		"type": "function"
+# 	},
+# 	{
+# 		"inputs": [
+# 			{
+# 				"internalType": "string",
+# 				"name": "productHash",
+# 				"type": "string"
+# 			},
+# 			{
+# 				"internalType": "uint256",
+# 				"name": "stockQuantity",
+# 				"type": "uint256"
+# 			},
+# 			{
+# 				"internalType": "uint256",
+# 				"name": "price",
+# 				"type": "uint256"
+# 			}
+# 		],
+# 		"name": "newProduct",
+# 		"outputs": [],
+# 		"stateMutability": "nonpayable",
+# 		"type": "function"
+# 	}
+# ]
 
-abi_request= [
-	{
-		"inputs": [
-			{
-				"internalType": "string",
-				"name": "productHash",
-				"type": "string"
-			},
-			{
-				"internalType": "address",
-				"name": "buyer",
-				"type": "address"
-			},
-			{
-				"internalType": "uint256",
-				"name": "quantity",
-				"type": "uint256"
-			},
-			{
-				"internalType": "contract Product",
-				"name": "productContract",
-				"type": "address"
-			}
-		],
-		"stateMutability": "payable",
-		"type": "constructor"
-	},
-	{
-		"stateMutability": "payable",
-		"type": "fallback"
-	},
-	{
-		"inputs": [],
-		"name": "_amountPayed",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "_quantity",
-		"outputs": [
-			{
-				"internalType": "uint256",
-				"name": "",
-				"type": "uint256"
-			}
-		],
-		"stateMutability": "view",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "acceptOffer",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"inputs": [],
-		"name": "cancelOffer",
-		"outputs": [],
-		"stateMutability": "nonpayable",
-		"type": "function"
-	},
-	{
-		"stateMutability": "payable",
-		"type": "receive"
-	}
-]
-store_contract = w3.eth.contract(address=Store_address, abi=abi_store)
+# abi_request= [
+# 	{
+# 		"inputs": [
+# 			{
+# 				"internalType": "string",
+# 				"name": "productHash",
+# 				"type": "string"
+# 			},
+# 			{
+# 				"internalType": "address",
+# 				"name": "buyer",
+# 				"type": "address"
+# 			},
+# 			{
+# 				"internalType": "uint256",
+# 				"name": "quantity",
+# 				"type": "uint256"
+# 			},
+# 			{
+# 				"internalType": "contract Product",
+# 				"name": "productContract",
+# 				"type": "address"
+# 			}
+# 		],
+# 		"stateMutability": "payable",
+# 		"type": "constructor"
+# 	},
+# 	{
+# 		"stateMutability": "payable",
+# 		"type": "fallback"
+# 	},
+# 	{
+# 		"inputs": [],
+# 		"name": "_amountPayed",
+# 		"outputs": [
+# 			{
+# 				"internalType": "uint256",
+# 				"name": "",
+# 				"type": "uint256"
+# 			}
+# 		],
+# 		"stateMutability": "view",
+# 		"type": "function"
+# 	},
+# 	{
+# 		"inputs": [],
+# 		"name": "_quantity",
+# 		"outputs": [
+# 			{
+# 				"internalType": "uint256",
+# 				"name": "",
+# 				"type": "uint256"
+# 			}
+# 		],
+# 		"stateMutability": "view",
+# 		"type": "function"
+# 	},
+# 	{
+# 		"inputs": [],
+# 		"name": "acceptOffer",
+# 		"outputs": [],
+# 		"stateMutability": "nonpayable",
+# 		"type": "function"
+# 	},
+# 	{
+# 		"inputs": [],
+# 		"name": "cancelOffer",
+# 		"outputs": [],
+# 		"stateMutability": "nonpayable",
+# 		"type": "function"
+# 	},
+# 	{
+# 		"stateMutability": "payable",
+# 		"type": "receive"
+# 	}
+# ]
+store_contract = w3.eth.contract(address=Store_address, abi=abis.abi_store)
+a = store_contract.functions
 
 #Coneccion con la db
 client = pymongo.MongoClient("mongodb+srv://dbUser:dbUser@cluster0.64zqb.mongodb.net/<dbname>?retryWrites=true&w=majority")

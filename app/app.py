6dd03f2bed80fd72e28ae92ef971b7ed169ffff6
Miguel Gonzalez <miguelgon_8@hotmail.com>
2020-11-06 22:26:18 -0600

    product handler

--- app/app.py
+++ app/app.py
@@ -15,8 +15,8 @@ store_address = configFile.store_address
 store_abi = abis.abi_store
 storeContract = w3.eth.contract(address=store_address, abi=store_abi)
 
-@app.route("/")
-@app.route("/products")                   # at the end point /
+@app.route("/",methods=['GET'])
+@app.route("/products",methods=['GET'])             # at the end point /
 def main():                      
     eventsFilter = storeContract.events.NewProduct.createFilter(fromBlock="0x0")
     events = eventsFilter.get_all_entries()
@@ -24,12 +24,17 @@ def main():
     content = ''
     for event in events:
         prodHash = event['args']['productHash']
-        availableStock = contract.getProductStock(prodHash)
+        try:
+            availableStock = contract.getProductStock(prodHash)
+        except :
+            availableStock = 0
         if availableStock>0:
             prodInfo = Productos.checkHash(prodHash)
             products.append(prodInfo)
     if len(products) == 0:
         content = 'No hay productos disponibles'
+    
+        #return render_template("catalogue/catalogue.html",products=products,content=content,title='Products',account=account)  
     return render_template("catalogue/catalogue.html",products=products,content=content,title='Products')
 
 @app.route("/products/<hashVar>")                   # at the end point /
@@ -62,11 +67,11 @@ def login():
         signature,h = Login.sign(msg,privKeyImp)
         verification = Login.verify(h,signature, accountAddress)
         if verification:
-            resp = make_response(render_template('base.html'))
+            resp = make_response(redirect(url_for('main')))
             resp.set_cookie('account',accountAddress)
             return resp
         else:
-            return render_template("login.html",palabra='Failure')
+            return render_template("login.html",palabra='attack at dawn')
     if request.method == 'GET':
         return render_template("login.html",palabra='attack at dawn')
 
